9b49781ca35895305d41c0b62cb6c909ffa23e12
Itachi-MacOS <warrior.006@inbox.ru>
2022-12-29 01:25:30 +0600

    MongoDB

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@src/controllers/app/app.controller';
 import { AppService } from '@src/services/app/app.service';
 import { UserModule } from './user.module';
+import { MongoDbModule } from '@src/modules/mongo-db.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, MongoDbModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from '@src/dto/user/create-user.dto';
 import { UpdateUserDto } from '@src/dto/user/update-user.dto';
+
+import { Model } from 'mongoose';
+import { User, UserSchema } from '@src/models/user.model';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async create(user: User): Promise<User> {
+    const newUser = new this.userModel(user);
+    return newUser.save();
+  }
+}
 
 @Injectable()
 export class UserService {
