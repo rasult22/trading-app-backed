3e0d96aff0eeeecccb19b0949f49bf9893a434c1
Itachi-MacOS <warrior.006@inbox.ru>
2022-12-29 00:52:22 +0600

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const options = new DocumentBuilder()
+  .setTitle('My API')
+  .setDescription('The API for my application')
+  .setVersion('1.0')
+  .addTag('users')
+  .build();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
